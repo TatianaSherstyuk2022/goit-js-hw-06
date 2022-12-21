3e443acb15ff7efdda49ba3e62e524ca5a402c9b
TatianaSherstyuk2022 <grinkevichtatiana@gmail.com>
2022-12-21 18:32:29 +0200

    add

---
+++ js/task-02.js
@@ -0,0 +1,13 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ingredientsListEl = document.querySelector('#ingredients')
+console.log(ingredientsListEl);
+
+const ingredientsItemEl = document.createElement('li')
